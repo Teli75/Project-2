5941a09ba284a1c64af40d6ea1b224d02e0d0c88
Teli75 <7telita@gmail.com>
2023-09-19 14:46:54 -0700

    Ready for meets expectations

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-const list = document.querySelector('.student-list');//same as author container
-const linkList = document.querySelector('link-list');
+const listContainer = document.querySelector('.student-list');//same as author container
+const linkList = document.querySelector('.link-list');
 const itemsPerPage = 9;
 /*
 For assistance:
@@ -20,23 +20,24 @@ function showPage(list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = (page * itemsPerPage) - 1;
 
-   const html = '';
+   listContainer.innerHTML = '';
 
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i <= endIndex) {
+
          const html = `
          <li class="student-item cf">
          <div class="student-details">
-            <img class="avatar" src="" alt="Profile Picture">
-            // <h3>${data[i][0]}/h3>
-            // <span class="email">${data[i][1]}</span>
+            <img class="avatar" src="${data[i].picture.medium}" alt="Profile Picture">
+            <h3>${data[i].name.first} ${data[i].name.last}</h3>
+            <span class="email">${data[i].email}</span>
          </div>
          <div class="joined-details">
-            <span class="date">Joined 12-15-2005</span>
+            <span class="date">${data[i].registered.date}</span>
          </div>
          </li>`;
          //Create the DOM elements needed to display the information for each matching student as you iterate over the list parameter. 
-         studentList.insertAdjacentHTML("beforeend", html);
+         listContainer.insertAdjacentHTML("beforeend", html);
       }
    }
 }
@@ -49,31 +50,42 @@ This function will create and insert/append the elements needed for the paginati
 function addPagination(list) {
    //select 9 students per page
    const numOfButtons = Math.ceil(list.length / itemsPerPage);
+
+   linkList.innerHTML= "";
    
    //Creates a button for every page
-   for (let i = 1; i <= numOfButtons.length; i++) {
+   for (let i = 1; i <= numOfButtons; i++) {
       const html = `
          <li>
-             <button type="button" class="active">${i}</button>
+             <button type="button" >${i}</button>
          </li>`;
 
       linkList.insertAdjacentHTML("beforeend", html);
-      //add active class to first element
-      linkList.querySelector("button").classList.add(".active");
+      
    }
+   //add active class to first element
+   linkList.querySelector("button").classList.add("active");
 }
 
 linkList.addEventListener("click", (event) => {
-   const activeButton = linkList.querySelector("active");
+   const activeButton = linkList.querySelector(".active");
    const buttonClicked = event.target.closest("button");
 
    if (buttonClicked) {
-      activeButton.classList.remove(".active");
-      buttonClicked.classList.add(".active");
+      activeButton.classList.remove("active");
+      buttonClicked.classList.add("active");
       showPage(data, buttonClicked.innerHTML);
    }
 });
 
+
+function addSearch(){
+   const html = `<label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" placeholder="Search by name...">
+   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+ </label>`;
+}
 
    // Call functions
    addPagination(data);
